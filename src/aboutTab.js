f7a8b3e465f0062016109edd072709af1833b8de
Draxgun <j.alejandro.banuelos@outlook.com>
2022-08-16 12:32:48 -0600

    About Section Done

--- src/aboutTab.js
+++ src/aboutTab.js
@@ -1,11 +1,14 @@
-import chefHeadMaya from './chefHeadMaya.png'
+import costcoMaya from './costcoMaya.jpeg'
 
 let loadAboutTab = () => {
     let contentContainer = document.getElementsByClassName('contentContainer')[0]
 
     let mainText = loadMainText()
+    let imageSide = loadImageSide()
+
 
     contentContainer.appendChild(mainText)
+    contentContainer.appendChild(imageSide)
 }
 
 let loadMainText = () => {
@@ -20,7 +23,7 @@ let loadMainText = () => {
 
     let mainText = document.createElement('div')
     mainText.classList.add("mainTextAbout");
-    mainText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum cursus mauris, et tristique tellus hendrerit ac. Maecenas nec lobortis urna. Vivamus molestie porta arcu, a tincidunt nunc auctor at.'
+    mainText.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum cursus mauris, et tristique tellus hendrerit ac. Maecenas nec lobortis urna. Vivamus molestie porta arcu, a tincidunt nunc auctor at.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod mi sit amet metus lobortis, id accumsan arcu consequat. Quisque ac velit cursus, laoreet dui eu, pharetra leo. Ut sit amet pharetra enim, id maximus neque. Etiam nec viverra mi, at bibendum dui. Phasellus ut efficitur nunc. In hac habitasse platea dictumst. Quisque congue sed nibh vel tristique. '
     textContainer.appendChild(mainText)
     
     return textContainer
@@ -29,8 +32,8 @@ let loadMainText = () => {
 
 let loadImageSide = () => {
     let homeImage = document.createElement('img');
-    homeImage.classList.add('homeImage')
-    homeImage.src = chefHeadMaya
+    homeImage.classList.add('aboutImage')
+    homeImage.src = costcoMaya
 
     return homeImage
 }
